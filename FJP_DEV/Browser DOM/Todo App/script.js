5d648d4da862e4821e047ca4d9e90e8079573203
Kishn0109 <raikaran159@gmail.com>
2022-03-24 02:28:45 +0000

    ticket ui done

--- FJP_DEV/Browser DOM/Todo App/script.js
+++ FJP_DEV/Browser DOM/Todo App/script.js
@@ -1,4 +1,14 @@
 let mybutton = document.querySelector(".toolbar-actionButton .addSection");
+let InputDiv = document.querySelector(".inputDiv");
+let flag = true;
+let lock = true;
 mybutton.addEventListener("click", function (e) {
-    console.log("clicked")
-})
+  console.log("clicked");
+  console.log(flag);
+  //   console.log(InputDiv.style);
+  flag = !flag;
+  flag == true
+    ? (InputDiv.style.display = "none")
+    : (InputDiv.style.display = "flex");
+});
+
