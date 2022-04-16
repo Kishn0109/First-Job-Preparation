5c18d6a735209f8f918e0a5d5d3aefe03063f288
Kishn0109 <raikaran159@gmail.com>
2022-04-16 09:50:40 +0530

    React using html Javascript

--- FJP_DEV/React/Class/Inheritance.js
+++ FJP_DEV/React/Class/Inheritance.js
@@ -1,7 +1,10 @@
 class Person {
-    constructor(name, color) {
-        this.name = name;
-        this.color = color;
+    constructor(name, age) {
+        (this.name = name), (this.age = age);
+    }
+
+    welcome() {
+        console.log("hello " + this.name)
     }
 }
 class Teacher extends Person {
@@ -12,17 +15,37 @@ class Teacher extends Person {
         super(name, color) //It is a keyword
         this.salary = salary;
     }
-}
-class Student extends Person {
-    constructor(name, color, rollno) {
-        // this.name = name;
-        // this.color = color;before 
-        // after 
-        super(name, color)
-        this.rollno = rollno
+
+    teacherfunc() {
+        super.welcome()
     }
 }
+// class Student extends Person {
+//     constructor(name, color, rollno) {
+//         // this.name = name;
+//         // this.color = color;before
+//         // after
+//         super(name, color)
+//         this.rollno = rollno
+//     }
+//     hello() {
+//         super.welcome()
+//     }
+// }
+class Student extends Person {
+    constructor(name, age, cgpa) {
+        super(name, age);
+        this.cgpa = cgpa;
+    }
 
-let techer = new Teacher("name", "white", 239);
-let truedent = new Student("name", "white", 239);
-console.log(techer, truedent)
+    hello() {
+        super.welcome()
+    }
+}
+let person1 = new Person("Adam", 25);
+
+// let techer = new Teacher("name", "white", 239);
+let student2 = new Student("Mark", 21, 8.0);
+// let truedent = new Student("name", "white", 239);
+console.log(student2.hello())
+// console.log(student1.welcome())
